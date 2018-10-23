bee89a0ae773151e8e6eb34933d3f7eea71b34c7
omolayole <omolayoleogunyemi@gmail.com>
2018-10-23 17:37:33 +0100

    Add Mapbox API key to this file

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoib21vbGF5b2xlIiwiYSI6ImNqbmxtNmJ1ajE4bWkzcHRkaXlwMmp2aTUifQ.BfPLRvD1NG15W-q0yBdChg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +

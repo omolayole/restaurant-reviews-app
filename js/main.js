2e90edff9131d77dfc41d3404ce07e346539bf65
omolayole <omolayoleogunyemi@gmail.com>
2018-10-26 16:55:38 +0100

    Add alt attribute to the img tag on this page

--- js/main.js
+++ js/main.js
@@ -160,6 +160,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = `Picture of ${restaurant.name} Restaurant`;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
